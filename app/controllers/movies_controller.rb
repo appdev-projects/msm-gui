04859e4d20c24b290a3fc1231ee63797733957e8
Kiowa Scott <97994294+kscott675@users.noreply.github.com>
2024-02-13 19:40:05 +0000

    delete movie function

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,4 +14,38 @@ class MoviesController < ApplicationController
 
     render({ :template => "movie_templates/show" })
   end
+
+  def create
+    #get user input
+    #create record
+    #populate each table
+    #save
+
+    #redirect back to /movies
+
+    #{"the_title"=>"1", "the_year"=>"2", "the_duration"=>"3", "the_description"=>"4", "the_image"=>"5", "the_director_id"=>"6"}
+
+    m = Movie.new
+
+    m.title = params[:the_title]
+    m.year = params[:the_year]
+    m.duration = params[:the_duration]
+    m.description = params[:the_description]
+    m.image = params[:the_image]
+    m.director_id = params[:the_director_id]
+
+    m.save
+
+    redirect_to("/movies")
+  end
+
+  def delete
+    movie_id = params[:an_id]
+
+    deleted_movie = Movie.where(id: movie_id).at(0)
+
+    deleted_movie.destroy
+
+    redirect_to("/movies")
+  end
 end
